fd0d945001d0eefbbafa73500d62e67280abe5cc
Winsler <r.makretski@gmail.com>
2019-03-03 20:15:12 +0300

    decision

--- src/index.js
+++ src/index.js
@@ -2,4 +2,32 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
-}
+
+    if (currency <= 0) return {};
+    if (currency > 10000) return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+
+    var coins = {
+    	50: "H",
+    	25: "Q",
+    	10: "D",
+    	5: "N",
+    	1: "P",
+    },
+    nominals = [],
+    change = {};
+
+    for (key in coins) nominals.push(key);
+    nominals.sort((a, b) => b - a);
+
+ 	for (let i = 0; i <= nominals.length; i++) {
+ 		if (currency <= 0) break;
+ 		let trunc = Math.trunc(currency / nominals[i]);
+ 		if (trunc > 0) {
+ 			change[coins[nominals[i]]] = trunc;
+ 			currency -= trunc * nominals[i];
+ 		};
+ 	};
+
+ 	return change; 
+
+};
